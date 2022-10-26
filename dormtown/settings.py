d92c85d0154fb028ee92c81c085b2f7b8730e734
6310682718 <6310682718@student.tu.ac.th>
2022-10-26 10:05:02 +0700

    update django heroku local

--- dormtown/settings.py
+++ dormtown/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +123,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CSRF_TRUSTED_ORIGINS = ['https://dormtownproj.herokuapp.com/']
+
+django_heroku.settings(locals())
