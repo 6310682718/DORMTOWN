e443cf57468c3b9d67fd49b3a0cb3a2b048f4fe6
6310682718 <6310682718@student.tu.ac.th>
2022-11-06 21:37:23 +0700

    initial for testcase

--- manager/tests.py
+++ manager/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
-
+from django.test import TestCase, SimpleTestCase, Client
+from django.urls import reverse, resolve
 # Create your tests here.
+from .views import *
+
+
+class TestUrl(SimpleTestCase):
+    def test_dashboard(self):
+        url = reverse("manager:dashboard")
+        self.assertEqual(resolve(url).func, index)
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        # initial account
+        account1 = {
+            'username': "admin",
+            'password': "admin",
+            'email': "admin@gmail.com",
+            'first_name': "AdminNaja",
+            'last_name': "LastnameAdmin"
+        }
+
+        self.new_user = User.objects.create_superuser(**account1)
