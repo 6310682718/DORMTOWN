de3cdbec7a9221bcdcbd19ff9be83e04b7acd59b
6310682718 <6310682718@student.tu.ac.th>
2022-11-06 16:13:37 +0700

    add subfix path

--- dormtown/urls.py
+++ dormtown/urls.py
@@ -19,11 +19,11 @@ from rooms import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('rooms.urls')),
-    path("occupant", include('occupant.urls')),
-    path("chat", include('chat.urls')),
-    path("manager", include("manager.urls")),
-    path("employee", include("employee.urls")),
-    path('users', include("users.urls")),
+    path("occupant/", include('occupant.urls')),
+    path("chat/", include('chat.urls')),
+    path("manager/", include("manager.urls")),
+    path("employee/", include("employee.urls")),
+    path('users/', include("users.urls")),
 ]
 handler404 = 'rooms.views.handler404'
 handler500 = 'rooms.views.handler500'
