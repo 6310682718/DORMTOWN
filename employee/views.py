a5cc03e853f449519f3abfea269786ee43b33526
6310682783 <69451473+6310682783@users.noreply.github.com>
2022-11-06 23:49:27 +0700

    clear employee/model file

--- employee/views.py
+++ employee/views.py
@@ -5,40 +5,13 @@ from .models import *
 
 # Create your views here.
 
-def index(request):
-    detail = {
-        'employee': {
-            'name': 'Kantapat Kowadisai',
-            'Tel': '0897827356',
-            'Address': '9330 W Lake Dr ## A, Eagle River, Alaska 99577, USA'}        
-        }
-    
-    return render(request, "employee/index.html", detail)
+def index(request):   
+    return render(request, "employee/index.html")
 
 def submit(rq):
-    detail = {
-        'report': {
-            'name': 'Cleaning Service',
-            'room': '3401',
-            'Tel': '0897827356',
-            'Duedate': '01/02/2022',
-            'assign_to_user_id':'0001',
-            'id' : '0001',
-            }        
-    }
-    return render(rq, "employee/submit.html",detail)
+    return render(rq, "employee/submit.html")
 
 def assign(rq):
-    detail = {
-        'report_id': {
-            'name': 'Cleaning Service',
-            'room': '3401',
-            'Tel': '0897827356',
-            'Duedate': '01/02/2022',
-            'assign_to_user_id':'0001',
-            'id' : '0001',
-            }        
-        }
-    return render(rq, "employee/assign.html",detail)
+    return render(rq, "employee/assign.html")
 
 
