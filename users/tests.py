100dcd9bd150faf25c403bfe4761b2f0dc6db3f5
6310682718 <6310682718@student.tu.ac.th>
2022-11-07 20:38:08 +0700

    edit some testcase users

--- users/tests.py
+++ users/tests.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from .views import *
 import datetime
 
+
 class TestUrl(SimpleTestCase):
     def test_login_is_resolved(self):
         url = reverse("users:login")
@@ -18,7 +19,22 @@ class TestUrl(SimpleTestCase):
         url = reverse("users:register")
         self.assertEqual(resolve(url).func, register)
 
+
 class TestView(TestCase):
+    def setUp(self):
+        self.username = 'newuser'
+        self.password = 'newuserpass'
+        self.email = 'newuser@dormtown.com'
+        self.first = 'New'
+        self.last = 'User'
+        self.credentials = {
+            'username': self.username,
+            'password': self.password,
+            'email': self.email,
+            'first_name': self.first,
+            'last_name': self.last}
+        self.new_user = User.objects.create_user(**self.credentials)
+
     def test_register(self):
         url = "/users/register"
         body = {
@@ -26,7 +42,7 @@ class TestView(TestCase):
             "firstname": "test",
             "lastname": "test",
             "password": "test1",
-            "confirm_password": "test1",
+            "confirm_password": "test12",
             "email": "test@gmail.com",
             "phone": "0963711479",
             "address": "test",
@@ -37,10 +53,13 @@ class TestView(TestCase):
             "zip": "1234",
         }
         response = self.client.post(url, body)
-        self.assertEqual(response.status_code, 400) 
+        self.assertEqual(response.status_code, 400)
 
     def test_login(self):
         url = "/users/login"
         body = {"username": "admin", "password": "admin1234"}
-        response = self.client.post(url, body)
-        self.assertEqual(response.status_code, 400) 
+        response_fail = self.client.post(url, body)
+        self.assertEqual(response_fail.status_code, 400)
+        # body = {"username": "admin", "password": "admin"}
+        # response = self.client.post(url, body)
+        # self.assertEqual(response.status_code, 200)
