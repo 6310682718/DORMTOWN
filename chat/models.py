ee70b628799f4d8e10ac95d1def5b4763bb9b303
6310682783 <69451473+6310682783@users.noreply.github.com>
2022-11-09 00:47:31 +0700

    add new file

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
+from occupant.models import Room
+class ChatRoom(models.Model):
+    room_name = models.CharField(max_length=10, default="all-rooms")
 
-# Create your models here.
+
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, related_name='messages', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+    ordering = ('date_added',)
