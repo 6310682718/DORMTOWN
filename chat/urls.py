a5b8202f1c54c8ef19457c88149bc532357713ec
6310682718 <6310682718@student.tu.ac.th>
2022-11-09 00:34:18 +0700

    custom url path

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,6 @@ app_name = "chat"
 
 urlpatterns = [
     path("", views.index, name="rooms"),
-    path('<int:room_number>/', views.room, name='chat'),
+    # path('<int:room_number>/', views.room, name='chat'),
+    path("<int:user_id>", views.chat_list, name="chat_list")
 ]
