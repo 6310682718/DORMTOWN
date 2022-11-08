3c3cc1113ccf29e72b5f64bac5a11bba31ae4751
6310682783 <69451473+6310682783@users.noreply.github.com>
2022-11-08 18:01:15 +0700

    edit index and views

--- employee/views.py
+++ employee/views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
-from .models import *
+from django.contrib.auth.models import User
+# from ..occupant.models import *
+from occupant.models import UserInfo
 
 # Create your views here.
 
-def index(request):   
-    return render(request, "employee/index.html")
+def index(request):
+    if not request.user.is_authenticated:
+        return render(request, 'users/login.html', status=400)
+    else:
+        user = User.objects.filter(pk=request.user.id).first()
+        user_info = UserInfo.objects.filter(user_id=request.user.id).first()
+        role_name = user_info.role_id.role_name
+        print(f"--> {user_info.role_id.role_name}")
 
-def submit(rq):
-    return render(rq, "employee/submit.html")
+        if role_name == 'Technician' or 'Housekeeper':
+            print("can_access = true")
+            can_access = True
+        else:
+            can_access = False
+            print("can_access = false")
 
-def assign(rq):
-    return render(rq, "employee/assign.html")
-
+        if(can_access):
+            return render(request, 'employee/index.html', {
+                'user': user,
+                'user_info': user_info,
+                'role_name': role_name
+            })
+        else:    
+            return render(request, "rooms/index.html")
 
