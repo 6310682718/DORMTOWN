72b3bc613755d9e98b834225a839969ca91c8635
6310682718 <6310682718@student.tu.ac.th>
2022-11-09 00:34:08 +0700

    custom routing websocket

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/<int:user_id>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/<str:user_id>/', consumers.ChatConsumer.as_asgi()),
 ]
