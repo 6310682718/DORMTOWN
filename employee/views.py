2aabb6c73c52c18781fc946dfc4dd027f9f61ae9
6310682783 <69451473+6310682783@users.noreply.github.com>
2022-11-10 14:18:28 +0700

    edit index and view

--- employee/views.py
+++ employee/views.py
@@ -14,8 +14,13 @@ def index(request):
         user = User.objects.filter(pk=request.user.id).first()
         user_info = UserInfo.objects.filter(user_id=request.user.id).first()
         role_name = user_info.role_id.role_name
-        report = Report.objects.filter(assign_to_id=request.user)
-        print("Reports : ", report)
+        report_a = Report.objects.filter(assign_to_id=request.user)
+        print("REPORT_A -----")
+        print(report_a)
+        report_na = Report.objects.filter(assign_to_id__isnull=True)
+        print("REPORT_NA -----")
+        print(report_na)
+
         if role_name == 'Technician' or 'Housekeeper':
             print("can_access = true")
             can_access = True
@@ -28,7 +33,8 @@ def index(request):
                 'user': user,
                 'user_info': user_info,
                 'role_name': role_name,
-                'report' : report,
+                'report_a' : report_a,
+                'report_na' : report_na,
             })
         else:    
             return render(request, "rooms/index.html")
