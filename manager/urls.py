b01fa7ffc6f0939628c299e3031cf3c7c176abfb
thanapat <thanapatspongpipat@gmail.com>
2022-11-11 18:12:27 +0700

    complete manager

--- manager/urls.py
+++ manager/urls.py
@@ -4,5 +4,14 @@ from django.urls import path
 app_name = "manager"
 urlpatterns = [
     path("", views.index, name="dashboard"),
-    path("rooms_available", views.rooms_available, name="rooms_available")
+    path("rooms_available", views.rooms_available, name="rooms_available"),
+    path("rooms_reserve", views.rooms_reserve, name="rooms_reserve"),
+    path("rooms_unavailable", views.rooms_unavailable, name="rooms_unavailable"),
+    path("employee_list", views.employee_list, name="employee_list"),
+    path("occupant_list", views.occupant_list, name="occupant_list"),
+    path("report_logs", views.report_logs, name="report_logs"),
+    path('edit_profile/<int:user_id>', views.edit_profile, name='edit_profile'),
+    path('delete_user/<int:user_id>', views.delete_user, name='delete_user'),
+
+
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from occupant.models import *
+from django.db.models import Q
+from django.urls import reverse
+
 # Create your views here.
 
 
@@ -9,4 +12,69 @@ def index(req):
     return render(req, "manager/dashboard.html")
 
 def rooms_available(req):
-    return render(req, "manager/available_rooms.html")
+    try:
+        rooms = Room.objects.filter(status=True)
+    except:
+        pass
+    return render(req, "manager/available_rooms.html", {"rooms": rooms})
+
+def rooms_reserve(req):
+    return render(req, "manager/reserve_rooms.html")
+
+def rooms_unavailable(req):
+    try:
+        rooms = Room.objects.filter(status=False)
+    except:
+        pass
+    return render(req, "manager/unavailable_rooms.html", {"rooms": rooms})
+
+def employee_list(req):
+    try:
+        user_info = UserInfo.objects.filter(Q(role_id=2) | Q(role_id = 3))
+    except:
+        pass
+    return render(req, "manager/employee_list.html", {"user_info": user_info})
+
+def occupant_list(req):
+    try:
+        user_info = UserInfo.objects.filter(Q(role_id=4) | Q(role_id = 5))
+    except:
+        pass
+    return render(req, "manager/employee_list.html", {"user_info": user_info})
+
+def report_logs(req):
+    try:
+        reports = Report.objects.all()
+    except:
+        pass
+    return render(req, "manager/report_logs.html", {"reports": reports})
+
+def edit_profile(request, user_id):
+    if not request.user.is_authenticated:
+        return render(request, 'users/login.html', status=403)
+    try:
+        user = User.objects.get(pk=user_id)
+        user_info = get_object_or_404(UserInfo, user_id=user_id)
+    except:
+        return render(request, 'rooms/500.html', status=500)
+
+    return render(request, 'occupant/edit_profile.html', {
+        'user': user,
+        'user_info': user_info,
+    })
+
+def delete_user(request, user_id):
+    if not request.user.is_authenticated:
+        return render(request, 'users/login.html', status=403)
+
+    try:
+        user = User.objects.get(pk=user_id)
+        user_info = get_object_or_404(UserInfo, user_id=user_id)
+    except:
+        return render(request, 'rooms/500.html', status=500)
+
+    if user is not None:
+        user_info.delete()
+        user.delete()
+
+    return redirect(reverse('manager:employee_list'))
