080727490ea53c7a03e5f3969d7bca0e0e0ee14d
6310682718 <6310682718@student.tu.ac.th>
2022-11-11 10:05:26 +0700

    prepare docker

--- dormtown/settings.py
+++ dormtown/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import django_heroku
+# import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -140,4 +140,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CSRF_TRUSTED_ORIGINS = ['https://git.heroku.com/dormtownproj.git']
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
