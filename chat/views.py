e45ba2c78a67b46b534854cd4f589dc175d642bf
6310682718 <6310682718@student.tu.ac.th>
2022-11-14 13:48:19 +0700

    hide comment code

--- chat/views.py
+++ chat/views.py
@@ -11,26 +11,10 @@ from django.contrib.auth.decorators import user_passes_test
 def index(req):
     chat_room, is_created = ChatRoom.objects.get_or_create(room_name=f"room-{req.user.id}")
     messages = Message.objects.filter(room=chat_room)
-    print("Messages : ", messages)
     return render(req, "chat/index.html", {
         "messages": messages
     })
 
-# @login_required
-# @user_passes_test(lambda u: u.is_superuser)
-# def chat_list(req, user_id):
-#     # For admin only
-#     try:
-#         room_name = f"room-{user_id}"
-#         chat_room, is_created = ChatRoom.objects.get_or_create(room_name=room_name)
-#         messages = Message.objects.filter(room=chat_room)
-#         text_to_admin = Message.objects.order_by().values('user').distinct()
-#         only_texter = Message.objects.filter(user__in=text_to_admin)
-#         for i in only_texter:
-#             print(f"USER : {i}")
-#         return render(req, "chat/chat_list.html", {"messages": messages, "only_texter" : only_texter})
-#     except Exception as e:
-#         print(f"<-- ERROR {e} -->")
 
 @login_required
 @user_passes_test(lambda u: u.is_superuser)
@@ -54,27 +38,5 @@ def chat_log(req, user_id):
         })
     except Exception as e:
         print(f"<-- ERROR {e} -->")
-# def rooms(request):
-#     rooms = Room.objects.all()
 
-#     return render(request, 'chat/rooms.html', {'rooms': rooms})
 
-# def room(request, room_number):
-#     print("<---- Room Page --->")
-#     try:
-#         room = Room.objects.get(room_number=room_number)
-#         messages = Message.objects.filter(room=room)[0:25]
-
-#         return render(request, 'chat/room.html', {'room': room, 'messages': messages})
-#     except Exception as e:
-#         print("Error from get room : ", e)
-#     obj = {
-#         "room_number": 0,
-#         "room_type": {
-#             "class_level": "A",
-#             "price": 6500,
-#             "room_service": 1
-#         },
-#         "status": True
-#     }
-#     return render(request, "chat/room.html", {"room": obj, "messages": []})
