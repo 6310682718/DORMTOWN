34b444bfbb401a2760fe86630caa74fa4babdd36
Natnicha Faksang <omsin.natnicha@gmail.com>
2022-11-18 15:57:30 +0700

    add sweet alert to occupant

--- occupant/views.py
+++ occupant/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 import datetime
 from .models import *
+import sweetify
 
 def index(request):
     if not request.user.is_authenticated:
@@ -67,6 +68,7 @@ def create_reserve(request, room_type):
             status_type=status_type
             )
 
+        sweetify.success(request, 'Reserve successful')
         return render(request, 'occupant/result_reserve.html', {
             'user_info': user_info,
             'room': reserve.room_type,
@@ -113,6 +115,7 @@ def delete_reserve(request, reserve_id):
     if reserve is not None:
         reserve.delete()
 
+    sweetify.success(request, 'Remove reservation successful')
     return redirect(reverse('occupant:index'))
 
 def report(request):
@@ -147,6 +150,7 @@ def report(request):
             role_id=None
         )
 
+        sweetify.success(request, 'Create report successful')
         return redirect(reverse('occupant:get_report', args=[report.id]))
     else:
         return render(request, 'occupant/report.html', {
@@ -181,6 +185,7 @@ def edit_report(request, report_id):
             note=note
         )
 
+        sweetify.success(request, 'Edit report successful')
         return redirect(reverse('occupant:list_report'))
     else:
         problem_type = ProblemType.objects.all()
@@ -243,4 +248,5 @@ def delete_report(request, report_id):
 
     report.delete()
     
+    sweetify.success(request, 'Delete report successful')
     return redirect(reverse('occupant:list_report'))
